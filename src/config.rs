0ff1587c74f2159e7a03556829f64b6cd462853f
Philipp Degler <deglerp@googlemail.com>
2022-09-04 20:42:47 +0200

    improve error message

--- src/config.rs
+++ src/config.rs
@@ -118,7 +118,7 @@ impl Config {
         if let Some(ref mut sync_settings) = self.sync_settings {
             if let Some(ref mut ftp_target_dir) = sync_settings.ftp_target_dir {
                 if !ftp_target_dir.starts_with('/') {
-                    println!("ftp_target_dir {ftp_target_dir} is treated as absolute path!");
+                    println!("ftp_target_dir contains no absolute path '/' will be prepended to '{ftp_target_dir}'");
                     ftp_target_dir.insert(0, '/');
                 }
             }
